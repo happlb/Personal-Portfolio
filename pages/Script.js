3efe296f3f6abdded621486642e8b74f2f522d0b
Lucy <happlb@msoe.edu>
2020-03-26 13:09:56 -0500

    made full page working on adding image

--- pages/Script.js
+++ pages/Script.js
@@ -8,20 +8,22 @@ function startTab() {
     document.getElementById("defaultOpen").click();
 
 }
+
   //opening tabs logic  
 function openTab(cityName, elmnt, color) {
     // Hide all elements with class="tabcontent" by default */
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+    tabcontent.forEach((tab) => {
+        tab.style.display = "none";
+    })
 
+    
     // Remove the background color of all tablinks/buttons
     tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
+    tablinks.forEach((tab) => {
+        tablinks.style.backgroundColor = "";
+    })
 
     // Show the specific tab content
     document.getElementById(cityName).style.display = "block";
