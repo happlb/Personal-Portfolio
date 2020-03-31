5c33611d95de51bdbfa1ec6cce0b9344354ae077
Lucy <happlb@msoe.edu>
2020-03-31 14:44:41 -0500

    workin on footer - trying to add white behind linkdin logo

--- pages/Script.js
+++ pages/Script.js
@@ -49,14 +49,5 @@ window.addEventListener('resize', () => {
 
 
 
-const myCarousel = new Carousel({
-    container: document.querySelector('.slider'),
-    items: document.querySelectorAll('.slider__item'),
-    displayControls: true,
-    controlsContainer: document.querySelector('.slider__controls'),
-    textControls: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
-    autoplay: true,
-    autoplayTime: 3500
-}); 
 
 
