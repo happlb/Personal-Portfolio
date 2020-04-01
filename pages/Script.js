4ea82773bc9fbdf7eb38cc8b3470cb8bae9635f3
Lucy <happlb@msoe.edu>
2020-03-31 20:48:30 -0500

    working on adding verticle tabs to portfolio

--- pages/Script.js
+++ pages/Script.js
@@ -10,7 +10,7 @@ function startTab() {
 }
 
   //opening tabs logic  
-function openTab(cityName, elmnt, color) {
+function openTab(tabName, elmnt, color) {
     // Hide all elements with class="tabcontent" by default */
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -25,11 +25,13 @@ function openTab(cityName, elmnt, color) {
     }
 
     // Show the specific tab content
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
 
     // Add the specific color to the button used to open the tab content
     elmnt.style.backgroundColor = color;
 }
+
+
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
@@ -50,4 +52,24 @@ window.addEventListener('resize', () => {
 
 
 
+/*portfolio tab functionality*/
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontentP, tablinksP;
 
+    // Get all elements with class="tabcontent" and hide them
+    tabcontentP = document.getElementsByClassName("tabcontentP");
+    for (i = 0; i < tabcontentP.length; i++) {
+        tabcontentP[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinksP = document.getElementsByClassName("tablinksP");
+    for (i = 0; i < tablinksP.length; i++) {
+        tablinksP[i].className = tablinksP[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
